d529766684b99fc2dce368b93c63d213bcce147e
LeonZerr <leonz99@web.de>
2025-02-12 14:10:08 +0100

    leaderboard added

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'stars')  
+    search_fields = ('username', 'email')  
+    list_filter = ('is_active', 'is_staff')  
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'stars', 'image')  
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
