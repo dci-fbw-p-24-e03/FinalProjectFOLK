547e2a3fff01d74e8eccc39b3828c275c1eca678
LeonZerr <leonz99@web.de>
2025-02-18 16:09:25 +0100

    shop modified

--- shop/models.py
+++ shop/models.py
@@ -4,12 +4,21 @@ from django.utils import timezone
 
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('joker', 'Joker'),
+        ('skins', 'Skins'),
+        ('opponents', 'Opponents'),
+    ]
+
     name = models.CharField(max_length=255)  
     description = models.TextField()  
     price = models.DecimalField(max_digits=10, decimal_places=2)  
     image = models.ImageField(upload_to="product_images/")  
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='skins')
+
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.category})"
+
 
 
 class Order(models.Model):
