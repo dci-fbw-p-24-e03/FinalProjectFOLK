b41fb48b9db98de911bd39b95fa612cef009dc20
Friederike L <friederike.lessi@web.de>
2025-02-27 14:17:02 +0100

    finished swapping

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("shop/", shop_view, name="shop_view"),
     path('buy/<int:product_id>/', buy_product, name='buy_product'),  
     path('confirm_purchase/<int:product_id>/', confirm_purchase, name='confirm_purchase'), 
-    path('shop-partial/', shop_swap, name='shop_swap'), 
+    path('shop-swap/', shop_swap, name='shop_swap'), 
+    path('buy-swap/<int:product_id>/', buy_product_swap, name='buy_product_swap'), 
+    path('confirm_purchase_swap/<int:product_id>/', confirm_purchase_swap, name='confirm_purchase_swap')
 ]
