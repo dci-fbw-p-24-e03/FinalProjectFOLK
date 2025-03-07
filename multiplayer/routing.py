0827367f807128a7bcadf3f6758bbcec9ea0adaf
KilianKlaiber <kilianklaiber@gmail.com>
2025-03-07 12:57:14 +0100

    connection to websocket working and message from front end to back end

--- multiplayer/routing.py
+++ multiplayer/routing.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from . import consumers
+from .consumers import ChatConsumer
+import re
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r"game-room/.*", ChatConsumer.as_asgi()),
 ]
