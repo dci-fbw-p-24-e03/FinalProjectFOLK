8a3adc8ad2b445ef25688f5d2beed4d128ef8124
Friederike L <friederike.lessi@web.de>
2025-03-07 15:42:38 +0100

    fixed defective tests in shop-app

--- AI_quiz/urls.py
+++ AI_quiz/urls.py
@@ -16,21 +16,20 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("",include("accounts.urls")),
-    path("",include("main.urls")),
-    path("",include("shop.urls")),
-    path("",include("game.urls")),
-    path('', include('multiplayer.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
- 
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("", include("accounts.urls")),
+        path("", include("main.urls")),
+        path("", include("shop.urls")),
+        path("", include("game.urls")),
+        path("", include("multiplayer.urls")),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
