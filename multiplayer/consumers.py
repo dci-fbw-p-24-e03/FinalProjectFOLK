93c0e3c79c7f93ad0e016c946bb7f89572f77eb6
KilianKlaiber <kilianklaiber@gmail.com>
2025-03-08 15:37:22 +0100

    Merging Hellchat group fixed + chat fixed

--- multiplayer/consumers.py
+++ multiplayer/consumers.py
@@ -16,7 +16,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # Identify the first game room, in which the user is registered.
         self.room_name = get_game_room(user)
 
-        # Create a chat group on the basis of the game room
+        # Create a group on the basis of the game room
         self.room_group_name = f"chat_{self.room_name}"
 
         # Join the group
@@ -30,7 +30,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         # Turn received data into a python readable dictionary
         data = json.loads(text_data)
-        
+
         print("received data: ", data)
         # Retrieve the chat message
         message = data.get("chat_message")
@@ -288,3 +288,58 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Send the message to the WebSocket
         await self.send(text_data=json.dumps({"message": message}))
+
+
+def get_game_room(username: str) -> str | None:
+    """Returng the first game room from the list of game rooms in the cache,
+    in which the user is registered.
+    """
+    # Get all the active game rooms in the cache
+    active_game_rooms = cache.get("active_game_rooms")
+    
+    if active_game_rooms == None:
+        return None
+
+    # Identify the first game room in the list, in which the user is.
+    # Select this room as chat room for this user.
+    player_names = []
+    for game_room in active_game_rooms:
+        room_content = cache.get(f"game_room:{game_room}")
+        if room_content == None:
+            continue
+        player_data = room_content["player_data"]
+        # Retrieve all the player names and add them to the list of players
+        for player in player_data:
+            player_names.append(player["username"])
+
+        if username in player_names:
+            return game_room
+    else:
+        return None
+
+
+def get_players(game_room: str) -> list[str] | None:
+    """ Return the list of users currently in the game room.
+    Example for the room content in the cache:
+    {'players': ['3', '2'], 
+    'player_data': [
+        {'id': 3, 'username': 'Ole', 'image': <ImageFieldFile: user_profile_images/profile_placeholder.jpeg>, 'average': 2.25}, 
+        {'id': 2, 'username': 'kilian', 'image': <ImageFieldFile: user_profile_images/0kIPXTKI_400x400.jpg>, 'average': 19.8}]
+        }
+    """
+
+    active_game_rooms = cache.get("active_game_rooms")
+    if game_room not in active_game_rooms:
+        return None
+
+    room_content = cache.get(f"game_room:{game_room}")
+    if room_content == None:
+        return None
+
+    player_data = room_content["player_data"]
+    player_names = []
+    for player in player_data:
+        player_names.append(player["username"])
+
+    return player_names
+
