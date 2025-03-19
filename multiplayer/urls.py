243b943cc817fa4fb4ec2fc8d6b2b19dc09a2225
Friederike <friederike.lessi@web.de>
2025-03-19 10:29:20 +0100

    edited styling to show both player

--- multiplayer/urls.py
+++ multiplayer/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import  results, multi_play, join_matchmaking, waiting_room, check_match, leave_matchmaking, game_room
+from .views import (
+    results,
+    multi_play,
+    join_matchmaking,
+    waiting_room,
+    check_match,
+    leave_matchmaking,
+    game_room,
+    test_multi_play_over,
+)
 
 urlpatterns = [
-    
     path("join-matchmaking/", join_matchmaking, name="join_matchmaking"),
     path("waiting-room/<str:user_id>/", waiting_room, name="waiting_room"),
     path("check-match/<str:user_id>/", check_match, name="check_match"),
@@ -10,4 +18,5 @@ urlpatterns = [
     path("game-room/<str:room_id>/", game_room, name="game_room"),
     path("multi_play/", multi_play, name="multi_play"),
     path("results/", results, name="results"),
+    path("test-multi-play-over/", test_multi_play_over, name="test_multi_play_over"),
 ]
